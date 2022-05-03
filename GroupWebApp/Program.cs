bf848caba327b61aaba305a713a05b07f5f567d3
Руслан Хакимов <Stud118292@stankin.ru>
2022-05-03 21:26:02 +0300

    Изменена стартовая страница

--- GroupWebApp/Program.cs
+++ GroupWebApp/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Categories}/{action=Main}/{id?}");
 
 app.Run();
