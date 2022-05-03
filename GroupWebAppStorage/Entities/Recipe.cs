79c1c42da0e6f19b710c16e8fcae0e1942c4cc4e
Руслан Хакимов <Stud118292@stankin.ru>
2022-05-03 17:24:08 +0300

    Добавлен контекст БД, поправлена логика работы таблиц, настроена строка подключения к бд

---
+++ GroupWebAppStorage/Entities/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GroupWebApp.Storage.Entities
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public byte Image { set; get; }
+        public string desc { set; get; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category {get; set;}
+    }
+
+}
