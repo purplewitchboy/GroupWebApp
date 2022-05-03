dc091e2d61c6bc9fac2f5ffd425b58503276a84f
Руслан Хакимов <Stud118292@stankin.ru>
2022-05-03 22:13:13 +0300

    Добавление интерфейса и его реализации к рецептам

---
+++ GroupWebApp.Logic/Recipes/IRecipeManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupWebApp.Logic.Recipes
+{
+    public interface IRecipeManager
+    {
+        Task<IList<Recipe>> GetAll();
+        Task Create(string name);
+    }
+}
