c69bd1188c4f958016bc15fd51f0b75fdc6f1c37
Ангелина Скокова <ang.skokova@gmail.com>
2022-05-14 20:25:53 +0300

    Фронт обновление

--- GroupWebAppStorage/Migrations/20220514165017_InitialCreate.Designer.cs
+++ GroupWebAppStorage/Migrations/20220514165017_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using GroupWebApp.Storage.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GroupWebApp.Storage.Migrations
 {
     [DbContext(typeof(RecipeContext))]
-    partial class RecipeContextModelSnapshot : ModelSnapshot
+    [Migration("20220514165017_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
